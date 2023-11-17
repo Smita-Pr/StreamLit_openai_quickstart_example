199c73cfec4e0490d2c875f4e838d3f030d76a42
Smita Prasad <87739068+Smita-Pr@users.noreply.github.com>
2023-11-17 18:44:14 +0000

    Update streamlit_quick_start_openai_example.py

--- streamlit_quick_start_openai_example.py
+++ streamlit_quick_start_openai_example.py
@@ -12,7 +12,7 @@ st.title('🦜🔗 Quickstart App')
 openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
 
 def generate_response(input_text):
-    llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+    llm = OpenAI(openai_api_key=openai_api_key)
     st.info(llm(input_text))
 
 with st.form('my_form'):
